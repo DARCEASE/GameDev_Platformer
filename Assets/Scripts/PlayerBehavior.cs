6b2fd2660d62b49fa658c7db2c53d978929bf4f4
Darcy <darcelisgutierrez@gmail.com>
2020-09-24 15:25:43 -0400

    Basic Mechanics
    Player can move and jump on platforms without double jumping and sticking to the side of plaltforms

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{ 
+    Rigidbody2D myRB;
+    float inputHor;
+    bool isJumping;
+    public float moveSpeed;
+    public float jumpPower;
+    public bool isGrounded;
+     
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHor = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded == true) 
+        {
+            isJumping = true;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        myRB.velocity = new Vector2(inputHor * moveSpeed, myRB.velocity.y);
+        if (isJumping == true)
+        {
+            myRB.velocity = new Vector2(myRB.velocity.x, jumpPower);
+            isJumping = false;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerGrounded.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrounded : MonoBehaviour
+{
+    public PlayerBehavior myChar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D activator)
+    {
+        myChar.isGrounded = true;
+    }
+    private void OnTriggerExit2D(Collider2D activator)
+    {
+        myChar.isGrounded = false;
+    }
+}
