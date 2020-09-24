c029e3a33c60b3dfee10b92cb675786438ef667c
Darcy <darcelisgutierrez@gmail.com>
2020-09-24 17:28:23 -0400

    Enemy Behavior Started
    Enemy placed and timer specifies movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform cameraFocusTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter2D(Collider2D activator)
+    {
+        cameraFocusTarget = activator.transform;
+    }
+   
+}
+                                                                                                                
---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public float walkTimer;
+    public Vector3 hor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walkTimer += Time.deltaTime;
+        if(walkTimer < 3)
+        {
+            //move left
+            hor = new Vector3(-3f, 0f, 0f);
+            GetComponent<Rigidbody2D>().AddForce(hor);
+        }
+        if (walkTimer > 3)
+        {
+            //move right
+            hor = new Vector3(3f, 0f, 0f);
+            GetComponent<Rigidbody2D>().AddForce(hor);
+        }
+    }
+}
